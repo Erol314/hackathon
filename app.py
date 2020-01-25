1f6c4d095df5c9d0f50474329f82fd3cde469826
Dana Mazur <dana.mazur.2000@mail.ru>
2020-01-25 19:20:45 +0200

    css

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from lib2to3.pytree import convert
+# from lib2to3.pytree import convert
 
-from flask import Flask, render_template
-import xmltodict
-import json
+from flask import Flask, render_template, request
+# import xmltodict
+# import json
 # from pprint import pprint
 #
 app = Flask(__name__)
-#
-
-from excel2json import convert_from_file
 
 
-EXCEL_FILE = r'C:\Users\sezgh\Downloads\Telegram Desktop\Hackathon\FCIM BUGET.xlsx # or '../example.xlsx'
-convert(EXCEL_FILE)
-
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', foo='ero')
+    colours = ['Facultatea Electronică și Telecomunicații', 
+        'Facultatea Energetică și Inginerie Electrică', 
+        'Facultatea Calculatoare, Informatică şi Microelectronică', 
+        'Facultatea Tehnologia Alimentelor',
+        'Facultatea Inginerie Mecanică',
+        'Industrială şi Transporturi',
+        'Facultatea Urbanism şi Arhitectură',
+        'Facultatea Construcții, Geodezie şi Cadastru',
+        'Facultatea Inginerie Economică şi Business',
+        'Facultatea Textile și Poligrafie']
+    return render_template('index.html', colours=colours)
+
 
 #
 # if __name__ == '__main__':
